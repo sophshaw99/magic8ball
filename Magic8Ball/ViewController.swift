83bfb5a8e14d417632dbb03166e331669a01b3c8
Sophia <sophshaw99@gmail.com>
2017-08-28 11:21:02 -0700

    initial commit

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var answerLbl : UILabel!
+    
+    let randomAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Signs point to yes.",
+    "Yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+    ]
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let randomIndex = Int(arc4random_uniform(UInt32(20)))
+        answerLbl.text = randomAnswers[randomIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
